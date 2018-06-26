954e63fe7841dd0c4922661d9a37e517ddbfc621
Dmitrii <javafvr@yahoo.com>
2018-06-26 18:05:33 +0500

    php-task-12

--- index.php
+++ index.php
@@ -6,16 +6,59 @@
 	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	<ol>
-		<?php
-			$books = array("451° по Фаренгейту", "Шантарам", "1984", "Мастер и Маргарита", "Три товарища", "Портрет Дориана Грея", "Вино из одуванчиков", "Цветы для Элджернона", "Над пропастью во ржи", "Маленький принц", "Анна Каренина", "Сто лет одиночества", "Тень горы", "Атлант расправил плечи");
-			
-			$sizeOfArray = count($books);
+<?php
 
-			for ($i=0; $i < $sizeOfArray; $i++) { 
-				echo "<li>$books[$i]";
-			}
-		?>
-	</ol>
+
+require 'db.php';
+
+// Создание новых записей
+
+// $course = R::dispense('courses');
+// $course->title = "Курс по React";
+// $course->tuts = 10;
+// $course->homeworks = 8;
+// $course->level = 'для новичков';
+// $course->price = 100;
+
+// R::store($course);
+
+//Получение всех записей
+
+// $courses = R::find('courses');
+
+// foreach ($courses as $course) {
+// 	// print_r($course);
+// 	echo "ID: " .$course->id . "<br>";
+// 	echo "Название: " .$course->title . "<br>";
+// 	echo "Количество уроков: " .$course->tuts . "<br>";
+// 	echo "Уровень: " .$course->level . "<br>";
+// 	echo "<hr>";
+// }
+
+//Обновление записи
+$course = R::load('courses', 3);
+// print_r($course);
+echo "ID: " .$course->id . "<br>";
+echo "Название: " .$course->title . "<br>";
+echo "Количество уроков: " .$course->tuts . "<br>";
+echo "Уровень: " .$course->level . "<br>";
+echo "Студентов: " .$course->students . "<br>";
+echo "Цена: " .$course->price . "<br>";
+echo "<hr>";
+
+$course->title = "React - ступень первая";
+$course->tuts = 14;
+$course->price = 80;
+$course->students = 20;
+$course->img = "<img src='sfdsd.jpg'>";
+
+R::store($course);
+
+// Удаление записи
+
+// $course = R::load('courses',2);
+// R::trash($course);
+
+?>
 </body>
 </html>
